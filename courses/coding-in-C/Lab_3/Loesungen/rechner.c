8f608640ca63b5a095dcd026f876f31cce9360fe
adamchrusciel23 <adamchrusciel2309@gmail.com>
2026-02-04 09:34:48 +0000

    made extra functions

--- courses/coding-in-C/Lab_3/Loesungen/rechner.c
+++ courses/coding-in-C/Lab_3/Loesungen/rechner.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
-
+int addition(int zahl1, int zahl2){
+    return zahl1 + zahl2;
+}
+int subtraktion(int zahl1, int zahl2){
+    return zahl1 - zahl2;
+}
+int multiplikation(int zahl1, int zahl2){
+    return zahl1 * zahl2;
+}
+int division(int zahl1, int zahl2){
+    return zahl1 / zahl2;
+}
 int main(){
     int ersteZahl;
     int zweiteZahl;
@@ -38,14 +49,30 @@ int main(){
         }
     }*/
     switch(rechenoperation){
-        case('+'):  ergebnis = ersteZahl + zweiteZahl;   printf("Ergebnis: %d", ergebnis);   break;
-        case('-'):  ergebnis = ersteZahl - zweiteZahl;   printf("Ergebnis: %d", ergebnis);   break;
-        case('*'):  ergebnis = ersteZahl * zweiteZahl;   printf("Ergebnis: %d", ergebnis);   break;
-        case('/'):  if(zweiteZahl){   ergebnis = ersteZahl / zweiteZahl;   printf("Ergebnis: %d", ergebnis);   break;}
-                    else{printf("\nDivision durch 0");}
-
+        case('+'):  
+            ergebnis = addition(ersteZahl, zweiteZahl);   
+            printf("Ergebnis: %d", ergebnis);   
+            break;
+        
+        case('-'):  
+            ergebnis = subtraktion(ersteZahl, zweiteZahl);   
+            printf("Ergebnis: %d", ergebnis);   
+            break;
+        
+        case('*'):  
+            ergebnis = multiplikation(ersteZahl, zweiteZahl);   
+            printf("Ergebnis: %d", ergebnis);   
+            break;
+        
+        case('/'): 
+            if(zweiteZahl){
+                ergebnis = division(ersteZahl, zweiteZahl);
+                printf("Ergebnis: %d", ergebnis);  
+            } 
+            else{
+                printf("Division durch 0");  
+            }  
+            break;
     }
-    
-
     return 0;
 }
