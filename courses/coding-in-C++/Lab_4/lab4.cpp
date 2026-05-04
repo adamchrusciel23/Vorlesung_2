4dded92a4a0347d55da2371ab31f22b02be8b998
adamchrusciel23 <adamchrusciel2309@gmail.com>
2026-05-04 13:16:14 +0000

    .

--- courses/coding-in-C++/Lab_4/lab4.cpp
+++ courses/coding-in-C++/Lab_4/lab4.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 class Vector2D{
     
@@ -9,19 +10,36 @@ class Vector2D{
     
         public:
         
-        Vectro2D(double x, double y) : x(x), y(y){}
+        Vector2D(double x, double y) : x(x), y(y) {}
         
-        double getX(){
+        double getX() const {
             return x;
         }
         
-        double getY(){
+        double getY() const {
             return y;
         }
         
-        void print(){
+        void print() const {
             std::cout   << "X: " << x
-                        << "Y: " << y << std::enl;
+                        << "Y: " << y << std::endl;
         }
         
-}
+        float getLength() const {
+            return sqrt(x*x + y*y);
+        }
+
+        float getLength(int precision) const {
+            float value = sqrt(x*x + y*y);
+            double factor = std::pow(10.0, precision);
+            return std::round(value * factor) / factor;
+        }
+
+        Vector2D operator+(const Vector2D &vector1) {
+            return Vector2D((this->x + vector1.getX()), (this->y + vector1.getY()));
+        }
+        void operator+=(const Vector2D &vector1) {
+            this->x += vector1.getX();
+            this->y += vector1.getY();
+        }
+};
